28dec4beb4e24683dd441b70769cffe45da43cdc
SethMack <sethvmack@gmail.com>
2023-07-04 16:26:25 +0100

    page setup before moving header content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#'>Skills</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://www.linkedin.com/in/seth-mack-b6ba50217/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/SethMack" target="_blank"><BsGithub/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Seth Mack. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,10 +6,18 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#'>Skills</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Seth Mack</h1>
-        <h5 className="text-light">Developer</h5>
+        <h5 className="text-light">Computer Science Student, Entrepreneur</h5>
         <CTA />
         <HeaderSocials />
 
