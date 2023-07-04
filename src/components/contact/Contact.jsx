3b30860cb84f51ff7b89f0e808192beb5caff24d
SethMack <sethvmack@gmail.com>
2023-07-04 14:44:23 +0100

    added contact jsx and css and linked up to emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
+import { useRef } from 'react'
+import emailjs, { send } from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_gz1cvoh', 'template_yj7ejkb', form.current, 'UBT2pYRvHYR2z_gRa')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>sethvmack@gmail.com</h5>
+            <a href='mailto:sethvmack@gmail.com' target='blank'>Send Me A Message</a>
+          </article>
+          <article className='contact__option'>
+            <BsLinkedin className='contact__option-icon'/>
+            <h4>LinkedIn Message</h4>
+            <h5>sethvmack@gmail.com</h5>
+            <a href='https://www.linkedin.com/in/seth-mack-b6ba50217/' target='blank'>Send Me A Message</a>
+          </article>
+        </div>
+        <form ref={form} on onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Messge' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
